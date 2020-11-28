f58af053bfb17c472c64185c54ac64de42603762
adityavkulkarni <adityavkulkarni0999@gmail.com>
2020-11-28 14:04:29 +0530

    added 2 testcases

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ class SemiColonTestCases(unittest.TestCase):
         def setUp(self):
                 self. semicolon = Semicolon()
                 self.solvents = Solvents()
+                #self. semicolon.launchsemicolon()
 
         def tearDown(self):
                 self.semicolon.close_driver()
 
+        def test_verify_home_page_classes(self):
+                semicolon=self.semicolon
+                semicolon.launchsemicolon()
+                solvents = self.solvents
+                self.assertTrue(solvents.verify_body_and_header_class('banner css-1qk4maf','css-8atqhb'))
+                semicolon.close_driver()
 
         def test_verify_home_page_nonuser(self):
                 semicolon=self.semicolon
-                solvents = self.solvents
                 semicolon.launchsemicolon()
-
+                solvents = self.solvents
                 #*************VERIFYING TEXT LABELS*************
                 self.assertTrue(solvents.is_tag_element_visible("Semicolon;",'a'))
                 self.assertTrue(solvents.is_tag_element_visible("Express your story.",'p'))
@@ -29,8 +35,11 @@ class SemiColonTestCases(unittest.TestCase):
                 self.assertTrue(solvents.is_tag_element_visible("EXPLORE",'p'))
                 self.assertTrue(solvents.is_tag_element_visible("REGISTER", 'p'))
                 #*************VERIFYING BUTTONS*************'
-                self.assertTrue(solvents.is_tag_element_visible("LOGIN;",'button'))
                 self.assertTrue(solvents.is_tag_element_visible("Explore",'button'))
+                self.assertTrue(solvents.verify_login_buttons())
+                # *************VERIFYING HEADER*************'
+                self.assertTrue(solvents.verify_header_nonuser())
+                semicolon.close_driver()
 
 if __name__ == '__main__':
     unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output=os.curdir+"/HTML_Reports"))
--- pageObjects.py
+++ pageObjects.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 import os
 
 options = webdriver.ChromeOptions()
@@ -22,11 +21,47 @@ class Semicolon():
 class Solvents(Semicolon):
         def __init__(self):
                 self.text_element = '//TAG[contains(text(),"LABEL")]'
-                #self.button_std = '//button[contains(text(),"LABEL")]'
-
+                self.login_button_1='//button[@class="css-1at8pli"][contains(text(),"LOGIN")]'
+                self.login_button_2 = '//button[@class="css-e62wgj"][contains(text(),"Login")]'
+                self.header_title = '//div[@id="root"]//header//h2//a[text()="Semicolon;"]'
+                self.header_links = '//div[@id="root"]//header//a[contains(@href,"LINK")]'
+                self.header_div = '//div[@id="root"]//header//parent::div'
+                self.body_div = '//div[@id="root"]//div[contains(@class,"banner")]'
         def is_tag_element_visible(self, label,tag):
                 driver.implicitly_wait(2000)
                 text_element = driver.find_element_by_xpath(self.text_element.replace("LABEL", label).replace('TAG',tag))
                 if text_element.is_displayed():
                         return True
                 return False
+
+        def verify_login_buttons(self):
+                driver.implicitly_wait(2000)
+                button1 = driver.find_element_by_xpath(self.login_button_1)
+                button2 = driver.find_element_by_xpath(self.login_button_2)
+                if button1.is_displayed() and button2 .is_displayed():
+                        return  True
+                return  False
+
+        def verify_header_nonuser(self):
+                driver.implicitly_wait(2000)
+                header_elements = []
+                header_elements.append(driver.find_element_by_xpath(self.header_title))
+                header_elements.append(driver.find_element_by_xpath(self.header_links.replace("LINK","blogs")))
+                header_elements.append(driver.find_element_by_xpath(self.header_links.replace("LINK", "register")))
+                header_elements.append(driver.find_element_by_xpath(self.header_links.replace("LINK", "login")))
+                for element in header_elements:
+                        if not element.is_displayed():
+                                return False
+                return  True
+
+        def verify_body_and_header_class(self,body_class,title_class):
+                driver.implicitly_wait(2000)
+                header = driver.find_element_by_xpath(self.header_div)
+                body = driver.find_element_by_xpath(self.body_div)
+                title_class_get = header.get_attribute('class')
+                body_class_get = body.get_attribute('class')
+                if title_class_get==title_class and body_class_get==body_class:
+                        return True
+                return False
+
+
